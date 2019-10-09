79865fb49dab7aa676d10197d98aafb6ab360410
Ahmed Alsawi <ahmedalsawi2012@gmail.com>
2019-10-09 21:20:54 +0200

    [Frontend] made PrivateRoute to add redirect to the props

--- frontend/src/components/utils/PrivateRoute.jsx
+++ frontend/src/components/utils/PrivateRoute.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
-const PrivateRoute = ({ component: Component, auth, ...rest }) => (
+const PrivateRoute = ({ component: Component, auth, redirect, ...rest }) => (
   <Route
     {...rest}
     render={props => {
       if (auth.isLoading) {
         return <h2>Loading...</h2>;
       } else if (!auth.isAuthenticated) {
-        return <Redirect to="/login" />;
+        return <Redirect to={redirect} />;
       } else {
         return <Component {...props} />;
       }
