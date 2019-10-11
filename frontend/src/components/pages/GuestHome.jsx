4e11747cd9fd118be5a547735c173b3d28ba6130
Ahmed Alsawi <ahmedalsawi2012@gmail.com>
2019-10-12 00:30:37 +0200

    [Frontend] Work on landing page

--- frontend/src/components/pages/GuestHome.jsx
+++ frontend/src/components/pages/GuestHome.jsx
@@ -1,14 +1,40 @@
 import React, { Component } from "react";
 
 import { Link } from "react-router-dom";
+
+import styled from "styled-components";
 export default class GuestHome extends Component {
   render() {
     return (
-      <div>
-        <h1>Landing</h1>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
-      </div>
+      <Wrapper>
+        <header className="navbar bg-light">
+          <Link className="navbar-brand" to="/">
+            ExpenTrak
+          </Link>
+          <div className="d-flex flex-row justify-content-between">
+            <Link to="/login" className="nav-item nav-link">
+              Login
+            </Link>
+            <Link to="/register" className="nav-item nav-link">
+              Register
+            </Link>
+          </div>
+        </header>
+        <body className="d-flex flex-column">
+          <div id="page-content">
+            <div className="jumbotron">
+              <h1 className="display-4">Hello, world!</h1>
+              <p className="lead"></p>
+            </div>
+          </div>
+        </body>
+        <footer id="sticky-footer" class="py-4 bg-dark text-white-50">
+          <div class="container text-center">
+            <small> &copy; EnpenTrak</small>
+          </div>
+        </footer>
+      </Wrapper>
     );
   }
 }
+const Wrapper = styled.div``;
