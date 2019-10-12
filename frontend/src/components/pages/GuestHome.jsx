111c63ca1b2ec0f3b299f6c9fa57602825686db5
Ahmed Alsawi <ahmedalsawi2012@gmail.com>
2019-10-12 09:32:18 +0200

    [Frontend] minor changes in css

--- frontend/src/components/pages/GuestHome.jsx
+++ frontend/src/components/pages/GuestHome.jsx
@@ -3,11 +3,12 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 import styled from "styled-components";
+
 export default class GuestHome extends Component {
   render() {
     return (
       <Wrapper>
-        <header className="navbar bg-light">
+        <header className="navbar bg-light sticky-top">
           <Link className="navbar-brand" to="/">
             ExpenTrak
           </Link>
@@ -20,21 +21,23 @@ export default class GuestHome extends Component {
             </Link>
           </div>
         </header>
-        <body className="d-flex flex-column">
-          <div id="page-content">
-            <div className="jumbotron">
-              <h1 className="display-4">Hello, world!</h1>
+        <body className="d-flex flex-column" id="page-content">
+          <div id="section1">
+            <div className="jumbotron m-0 rounded-0">
+              <h1 className="display-4">Track your Expenses easily</h1>
               <p className="lead"></p>
             </div>
           </div>
+          <div id="section2"></div>
         </body>
         <footer id="sticky-footer" class="py-4 bg-dark text-white-50">
           <div class="container text-center">
-            <small> &copy; EnpenTrak</small>
+            {/* <small> &copy; EnpenTrak</small> */}
           </div>
         </footer>
       </Wrapper>
     );
   }
 }
+
 const Wrapper = styled.div``;
